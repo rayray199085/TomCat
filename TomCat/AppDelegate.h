9bee447958cb1de7c392f59ff8b96e62752c9e76
Rui Cao <rayray199085@gmail.com>
2019-02-04 16:11:58 +1100

    Initial Commit

---
+++ TomCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TomCat
+//
+//  Created by Stephen Cao on 4/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TomCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TomCat
+//
+//  Created by Stephen Cao on 4/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
